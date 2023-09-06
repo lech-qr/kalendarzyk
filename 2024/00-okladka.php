c8319b2c3009ca095ec9c1619a11334aa40d4960
Lech Kurant <lech@jet-it.pl>
2023-09-06 14:46:50 +0200

    Roszada okładki

--- 2024/00-okladka.php
+++ 2024/00-okladka.php
@@ -9,18 +9,6 @@
         </div>
     </article>
     <article class="prawa wzor_A">
-        <div class="kolor"></div>
-        <div class="tresc">
-            <h4>Szema Izrael<br>Adonaj Elohenu<br>Adonaj Ehad</h4>
-            <img class="szema" src="../images/shema.jpg" alt="Szema" >
-            <h4>Słuchaj Izraelu<br>Pan jest naszym Bogiem<br>Pan jest Jedyny</h4>
-            <img class="kerygma" src="../images/kerygma-okladka.svg" >
-            <h5>tel. kom. +48 609 777 777<br>e-mail: sklep@centrumliturgiczne.pl</h5>
-        </div>
-    </article>
-</section>
-<section id="okladka-2">
-    <article class="lewa wzor_A">
         <div class="tresc">    
             <h2>Kochany Użytkowniku<br>kalendarzyka liturgicznego,<br>nasz Darczyńco!</h2>
             <p>Serdecznie&nbsp;dziękujemy&nbsp;za&nbsp;okazanie&nbsp;wsparcia Fundacji&nbsp;Godne&nbsp;Życie,&nbsp;a&nbsp;zwłaszcza&nbsp;za&nbsp;przekazanie nam&nbsp;1,5%&nbsp;podatku&nbsp;i&nbsp;darowizny.&nbsp;Szczególnie&nbsp;dziękujemy za&nbsp;kwoty&nbsp;bardzo&nbsp;niskie,&nbsp;ale&nbsp;darowane&nbsp;z&nbsp;sercem. Te&nbsp;niewielkie&nbsp;i&nbsp;te&nbsp;duże&nbsp;pieniądze&nbsp;pomagają&nbsp;utrzymać Misyjne&nbsp;Seminarium&nbsp;Redemptoris&nbsp;Mater&nbsp;i&nbsp;tyle&nbsp;dobrych dzieł&nbsp;bez&nbsp;doznawania&nbsp;jakiegokolwiek&nbsp;uszczerbku na&nbsp;majątku&nbsp;własnym,&nbsp;ale&nbsp;trzeba&nbsp;się&nbsp;potrudzić i&nbsp;prawidłowo&nbsp;wypełnić&nbsp;PIT-a&nbsp;z&nbsp;wpisaniem&nbsp;numeru KRS&nbsp;naszej&nbsp;Fundacji:&nbsp;0000083782&nbsp;-&nbsp;Fundacja&nbsp;Godne&nbsp;Życie z&nbsp;siedzibą&nbsp;w&nbsp;Lublinie.&nbsp;Prosimy&nbsp;o&nbsp;pomoc&nbsp;w&nbsp;przekonaniu innych&nbsp;osób&nbsp;do&nbsp;wsparcia&nbsp;naszej&nbsp;Fundacji,&nbsp;że&nbsp;to,&nbsp;co&nbsp;robimy ma&nbsp;głęboki&nbsp;sens.&nbsp;Prosimy&nbsp;o&nbsp;przekazanie&nbsp;nam&nbsp;1,5%&nbsp;podatku, więcej,&nbsp;prosimy&nbsp;także&nbsp;o&nbsp;darowizny,&nbsp;które&nbsp;można&nbsp;wpłacać na&nbsp;załączonym&nbsp;blankiecie.</p>
@@ -29,8 +17,10 @@
             <p>www.fundacja.org.pl</p>
         </div>
     </article>
-    <article class="prawa wzor_A">
-        <div class="tresc">
+</section>
+<section id="okladka-2">
+    <article class="lewa wzor_A">
+    <div class="tresc">
             <h3>CENTRUM LITURGICZNE</h3>
             <p>Centrum Liturgiczne zaopatruje wspólnoty<br>neokatechumenalne w Polsce w:</p>
 
@@ -56,6 +46,16 @@
 
             <h4>Konto bankowe:</h4>
             <p>Tomasz Bojarski PPHU TiBi<br>20-089 Lublin, ul. Biernackiego 22<br>Nr: 81 1020 3150 0000 3502 0143 9512<br>kod BIC (SWIFT): BPKOPLPW</p>
+        </div>
+    </article>
+    <article class="prawa wzor_A">
+        <div class="kolor"></div>
+        <div class="tresc">
+            <h4>Szema Izrael<br>Adonaj Elohenu<br>Adonaj Ehad</h4>
+            <img class="szema" src="../images/shema.jpg" alt="Szema" >
+            <h4>Słuchaj Izraelu<br>Pan jest naszym Bogiem<br>Pan jest Jedyny</h4>
+            <img class="kerygma" src="../images/kerygma-okladka.svg" >
+            <h5>tel. kom. +48 609 777 777<br>e-mail: sklep@centrumliturgiczne.pl</h5>
         </div>
     </article>
 </section>
