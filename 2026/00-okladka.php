3c8e2d934bedfe80f31c73f7b444fc6ddbe987d9
Lech <lech@jet-it.pl>
2025-09-11 08:36:41 +0200

    Poprawki na okładce

--- 2026/00-okladka.php
+++ 2026/00-okladka.php
@@ -11,7 +11,21 @@
     <article class="prawa wzor_A srodek">
         <div class="tresc">    
             <h2>Kochany Użytkowniku<br>kalendarzyka liturgicznego,<br>nasz Darczyńco!</h2>
-            <p>Serdecznie&nbsp;dziękujemy&nbsp;za&nbsp;okazanie&nbsp;wsparcia Fundacji&nbsp;Godne&nbsp;Życie,&nbsp;a&nbsp;zwłaszcza&nbsp;za&nbsp;przekazanie nam&nbsp;1,5%&nbsp;podatku&nbsp;i&nbsp;darowizny.&nbsp;Szczególnie&nbsp;dziękujemy za&nbsp;kwoty&nbsp;bardzo&nbsp;niskie,&nbsp;ale&nbsp;darowane&nbsp;z&nbsp;sercem. Te&nbsp;niewielkie&nbsp;i&nbsp;te&nbsp;duże&nbsp;pieniądze&nbsp;pomagają&nbsp;utrzymać Misyjne&nbsp;Seminarium&nbsp;Redemptoris&nbsp;Mater&nbsp;i&nbsp;tyle&nbsp;dobrych dzieł&nbsp;bez&nbsp;doznawania&nbsp;jakiegokolwiek&nbsp;uszczerbku na&nbsp;majątku&nbsp;własnym,&nbsp;ale&nbsp;trzeba&nbsp;się&nbsp;potrudzić i&nbsp;prawidłowo&nbsp;wypełnić&nbsp;PIT-a&nbsp;z&nbsp;wpisaniem&nbsp;numeru KRS&nbsp;naszej&nbsp;Fundacji:&nbsp;0000083782&nbsp;-&nbsp;Fundacja&nbsp;Godne&nbsp;Życie z&nbsp;siedzibą&nbsp;w&nbsp;Lublinie.&nbsp;Prosimy&nbsp;o&nbsp;pomoc&nbsp;w&nbsp;przekonaniu innych&nbsp;osób&nbsp;do&nbsp;wsparcia&nbsp;naszej&nbsp;Fundacji,&nbsp;że&nbsp;to,&nbsp;co&nbsp;robimy ma&nbsp;głęboki&nbsp;sens.&nbsp;Prosimy&nbsp;o&nbsp;przekazanie&nbsp;nam&nbsp;1,5%&nbsp;podatku, więcej,&nbsp;prosimy&nbsp;także&nbsp;o&nbsp;darowizny,&nbsp;które&nbsp;można&nbsp;wpłacać na&nbsp;załączonym&nbsp;blankiecie.</p>
+            <p>Serdecznie&nbsp;dziękujemy&nbsp;za&nbsp;okazanie&nbsp;wsparcia
+            Fundacji&nbsp;Godne&nbsp;Życie,&nbsp;a&nbsp;zwłaszcza&nbsp;za&nbsp;przekazanie
+            nam&nbsp;1,5%&nbsp;podatku&nbsp;i&nbsp;darowizny.&nbsp;Szczególnie&nbsp;dziękujemy
+            za&nbsp;kwoty&nbsp;bardzo&nbsp;niskie,&nbsp;ale&nbsp;darowane&nbsp;z&nbsp;sercem.&nbsp;Te
+            niewielkie,&nbsp;a&nbsp;jednak&nbsp;duże&nbsp;pieniądze&nbsp;pomagają&nbsp;utrzymać
+            Misyjne&nbsp;Seminarium&nbsp;Redemptoris&nbsp;Mater&nbsp;wiele
+            dobrych&nbsp;dzieł&nbsp;bez&nbsp;doznawania&nbsp;jakiegokolwiek
+            uszczerbku&nbsp;na&nbsp;majątku&nbsp;własnym,&nbsp;ale&nbsp;trzeba&nbsp;się
+            potrudzić&nbsp;i&nbsp;prawidłowo&nbsp;wypełnić&nbsp;PIT-a&nbsp;z&nbsp;wpisaniem
+            numeru&nbsp;KRS&nbsp;naszej&nbsp;Fundacji:&nbsp;0000083782&nbsp;-&nbsp;Fundacja
+            Godne&nbsp;Życie&nbsp;z&nbsp;siedzibą&nbsp;w&nbsp;Lublinie.&nbsp;Prosimy&nbsp;o&nbsp;pomoc
+            w&nbsp;przekonaniu&nbsp;innych&nbsp;osób&nbsp;do&nbsp;wsparcia&nbsp;naszej&nbsp;Fundacji,
+            że&nbsp;to,&nbsp;co&nbsp;robimy&nbsp;ma&nbsp;głęboki&nbsp;sens.&nbsp;Prosimy&nbsp;o&nbsp;przekazanie
+            nam&nbsp;1,5%&nbsp;podatku,&nbsp;więcej,&nbsp;prosimy&nbsp;także&nbsp;o&nbsp;darowizny,
+            które&nbsp;można&nbsp;wpłacać&nbsp;na&nbsp;nasze&nbsp;konto&nbsp;podane&nbsp;na&nbsp;dole.</p>
             <p>Za każdy dar z serca dziękujemy.<br><strong>Prezes Zarządu - Tomasz Bojarski</strong><br>tb@fundacja.org.pl</p>
             <p><strong>ORGANIZACJA POŻYTKU PUBLICZNEGO<br>Fundacja Godne Życie - KRS 0000083782</strong><br>20-089 Lublin, ul. Biernackiego 22<br>konto bankowe: <strong>21 2030 0045 1110 0000 0095 6050</strong></p>
             <p>www.fundacja.org.pl</p>
